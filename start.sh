b79ccb4c0ebcc6fc41001a7b4849078cef491bcf
sulaimangari <sulaimangari97@gmail.com>
2025-05-19 14:17:41 +0700

    change to tmate

--- start.sh
+++ start.sh
@@ -2,11 +2,9 @@ sudo apt update
 sudo apt install linux-modules-extra-`uname -r`
 sudo modprobe binder_linux devices="binder,hwbinder,vndbinder"
 sudo modprobe ashmem_linux
-sudo mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
-sudo apt update && sudo apt install cloudflared openssh-server
-sudo systemctl enable ssh --now
-sudo systemctl start ssh
+# sudo mkdir -p --mode=0755 /usr/share/keyrings
+# curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+# echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+# sudo apt update && sudo apt install cloudflared
 sudo sysctl -w net.core.rmem_max=7500000
 sudo sysctl -w net.core.wmem_max=7500000

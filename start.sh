fec0584e0b7fa4f9965cf5d352d51c9b9200ae8b
sulaimangari <sulaimangari97@gmail.com>
2025-05-19 12:56:13 +0700

    fix apt

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ modprobe ashmem_linux
 sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
-sudo apt-get install cloudflared
+sudo apt update && sudo apt install cloudflared

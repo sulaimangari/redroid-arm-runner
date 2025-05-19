3c76e21e8e12ecceecdd4364ed109236dc5eaa3c
sulaimangari <sulaimangari97@gmail.com>
2025-05-19 13:00:54 +0700

    fix buffer size

--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 sudo apt update && sudo apt install cloudflared
+sysctl -w net.core.rmem_max=7500000
+sysctl -w net.core.wmem_max=7500000
